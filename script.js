79f637929686b4d83e324cf11c24a59b5fa2f2b1
tahsinaltabib <nabilarah33@gmail.com>
2025-06-21 23:18:54 +0600

    some code update

--- script.js
+++ script.js
@@ -1,22 +1,22 @@
 let age=document.querySelector(".age")
-let salery=document.querySelector(".salery")
+let selary=document.querySelector(".selary")
 let home=document.querySelector(".home")
 let button=document.querySelector("button")
 let output=document.querySelector(".output")
 
 
 button.addEventListener('click',function(){
-    if(!(age.value,salery.value,home.value)){
+    if(!(age.value,selary.value,home.value)){
         output.style.display="block"
         output.style.color="rgb(253, 249, 3)"
-        output.innerHTML="Age form filaf kor"
+        output.innerHTML="Aage form fillup koro!"
 
     }else  if(!(age.value>=20)){
         output.style.display="block"
         output.style.color="rgb(253, 3, 3)"
-        output.innerHTML="age boro o beta"
+        output.innerHTML="Aage boro hoow Baba!"
         
-    }else if(!(salery.value>=45000)){
+    }else if(!(selary.value>=45000)){
         output.style.display="block"
         output.innerHTML="Amdr meye k biye krte hole minimum 45000 selary thakte hobe!"
         output.style.color="rgb(253, 3, 3)"
@@ -28,7 +28,7 @@ button.addEventListener('click',function(){
     }else{
         output.style.display="block"
         output.innerHTML="Biya korte parbe"
-         output.style.color="Green"
+         output.style.color="#38dc30"
     }
         
 
